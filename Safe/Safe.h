d1cd06fcad68ad26351a1b3477c7c38221534079
Mohamed Maged <1902228@eng.asu.edu.eg>
2021-12-07 00:22:33 +0200

    Update code

--- Safe/Safe.h
+++ Safe/Safe.h
@@ -15,15 +15,6 @@
 #define EEPROM_PASSWORD_LOCATION 0x21
 #define PASSWORD_LENGTH 4
 #define MAX_TRIES 3
-char arr[PASSWORD_LENGTH];
-char set[PASSWORD_LENGTH];
-char repeat[PASSWORD_LENGTH];
-char setting_tries = MAX_TRIES;
-char check_tries = MAX_TRIES;
-char pressed_value = NOTPRESSED;
-char i,flag;
-char Check_flag=0;
-char setting_flag;
 void Set_Pass();//This function enables the user to set a password to the safe
 void Repeat_Pass();//This function asks the user to repeat the same password he has set
 /*
